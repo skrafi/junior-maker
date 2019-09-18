c842bb116dee044f0fbaa5d3bac9d9e54f81a484
Maciej-Dusza <maciej-dusza@wp.pl>
2019-09-18 21:45:42 +0200

    Header first step css formating

--- src/admin/admin.jsx
+++ src/admin/admin.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 export class Admin extends React.Component {
     render() {
         return (
-            <div> "Admin"</div >
+            <div>
+                "Admin"
+            </div >
         )
     }
 };
